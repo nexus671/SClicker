947b312ebe883fd402cf230a3578567f399b5aca
Ahmane <acurrymuir@gmail.com>
2017-06-02 14:35:13 -0400

    init

---
+++ android/src/com/neonexus671/speedclicker/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.neonexus671.speedclicker;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.neonexus671.speedclicker.SClicker;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SClicker(), config);
+	}
+}
---
+++ desktop/src/com/neonexus671/speedclicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.neonexus671.speedclicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.neonexus671.speedclicker.SClicker;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SClicker(), config);
+	}
+}
